24110b93ab37b1c611a21580213dfa2b5906ab2a
Marimariyap <speed3215@gmail.com>
2023-03-03 16:07:55 +0900

    main Template

---
+++ src/components/TodoListTemplate.js
@@ -0,0 +1,14 @@
+import styles from "./TodoListTemplate.module.css";
+
+const TodoListTemplate = ({ form, children, palette }) => {
+  return (
+    <main className={styles.todolisttemplate}>
+      <div className={styles.title}>오늘 할 일</div>
+      <section className={styles.palettewrapper}>{palette}</section>
+      <section className={styles.formwrapper}>{form}</section>
+      <section className={styles.todoswrapper}>{children}</section>
+    </main>
+  );
+};
+
+export default TodoListTemplate;
